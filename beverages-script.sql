7b6149bfe9312688b72cd656b6910afd0f14ff89
Teresa de Castro <teresa.decastro@hotmail.com>
2024-04-28 20:18:11 -0400

    Inner joined tables and added screenshot

--- beverages-script.sql
+++ beverages-script.sql
@@ -1,21 +1,15 @@
 USE beverages;
+
+SELECT 
+    st.TeaID, t.TeaName, st.SodaID, s.SodaName
+FROM
+    SodaTeas st
+            INNER JOIN
+    Teas t ON st.TeaID = t.TeaID
+        INNER JOIN
+    Sodas s ON s.SodaID = st.SodaID;
+
+
 SELECT * FROM Sodas;
-SELECT * FROM Teas;
-
--- SELECT
--- 	*
--- FROM 
--- 	Sodas s
--- 		INNER JOIN 
--- 	Teas t ON s.SodaID = t.SodaID; 
-
--- SELECT 
--- 	t.TeaName, 
---     s.SodaName
--- FROM
--- 	Sodas s
--- 		INNER JOIN
--- 	Teas t ON s.SodaID = t.SodaID; 
-    
+SELECT * FROM Teas;     
 SELECT * FROM SodaTeas;
-
